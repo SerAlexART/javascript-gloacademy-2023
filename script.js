c823a30e3673ea5e75e673ff8855215308adf2bc
Sergey Alekseev <s_alekseev@lesta.group>
2023-11-28 15:08:14 +0300

    Выполнение 2 задания

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptiv;
+let title = 'JavaScript - 2023/2024';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 5.555;
+let rollback = 55;
+let fullPrice = 555.555;
+let adaptiv = true;
 
-console.log('First log.');
+screens = screens.toLocaleLowerCase().split(', ');
 
-alert('Important!');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptiv);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+console.log(screens);
+console.log(fullPrice * (rollback / 100));
+
+// console.log('First log.');
+// alert('Important!');
