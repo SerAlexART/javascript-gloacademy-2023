33abf3c43ec840f6f0d06b3c2929fd1d7f3e3a8c
SerAlexART <wolfi-5@mail.ru>
2023-11-29 23:04:15 +0300

    Выполнение 3 задания

--- script.js
+++ script.js
@@ -1,20 +1,83 @@
-let title = 'JavaScript - 2023/2024';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 5.555;
-let rollback = 55;
-let fullPrice = 555.555;
-let adaptiv = true;
+// Привет! =) Знаю, что нужно изначально задавать переменные, а потом с ними работать, но в рамках выполнения домашних работ и их проверки, я решил временно разбить на блоки. Если это строго не рекомендуется, то просьба сообщить и я поправлю, спасибо!
 
-screens = screens.toLocaleLowerCase().split(', ');
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptiv);
-console.log(screens.length);
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
-console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+
+// 3) Спрашиваем у пользователя “Как называется ваш проект?” и результат сохраняем в переменную title
+let title = prompt('Как называется ваш проект?');
+
+console.log('Название проекта ' + '"' + title + '".');
+
+
+
+//4) Спросить у пользователя “Какие типы экранов нужно разработать?” сохранить в переменную screens (пример: "Простые, Сложные, Интерактивные")
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные').toLocaleLowerCase().split(', ');
+
 console.log(screens);
-console.log(fullPrice * (rollback / 100));
+
+
+
+// 5) Спросить у пользователя “Сколько будет стоить данная работа?” и сохранить в переменную screenPrice (пример: 12000)
+let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+
+console.log('Данная работа будет стоить ' + screenPrice + 'р.');
+
+
+// 6) Спросить у пользователя “Нужен ли адаптив на сайте?” и сохранить данные в переменной adaptive (булево значение true/false)
+let adaptiv = Boolean(prompt('Нужен ли адаптив на сайте?'));
+console.log(typeof adaptiv);
+console.log(adaptiv == true ? console.log('Адаптив нужен.') : console.log('Адаптив не нужен.'));
+
+
+// 7) Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные 1. “Какой дополнительный тип услуги нужен?” (например service1, service2) 2. “Сколько это будет стоить?” (например servicePrice1, servicePrice2) в итоге 4 вопроса и 4 разные переменных, вопросы задаются в такой последовательности Название - Стоимость - Название - Стоимость
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Вёрстка модального окна');
+let servicePrice1 = +prompt('Сколько это будет стоить?', '500');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Написание скриптов для модального окна');
+let servicePrice2 = +prompt('Сколько это будет стоить?', '555');
+console.log(service1 + ' — стоимость ' + servicePrice1 + 'р.\n' + service2 + ' — стоимость ' + servicePrice2 + 'р.');
+
+
+// 8) Вычислить итоговую стоимость работы учитывая стоимость верстки экранов и дополнительных услуг (screenPrice + servicePrice1 + servicePrice2) и результат занести в переменную fullPrice
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+console.log(typeof fullPrice);
+console.log('Итоговая стоимость будет равна ' + fullPrice + 'р.');
+
+
+// 9) Объявить переменную servicePercentPrice и занести в нее итоговую стоимость за вычетом отката посреднику (servicePercentPrice = fullPrice - Откат посреднику), округлив результат в большую сторону (методы объекта Math в помощь). Вывести servicePercentPrice в консоль.
+let rollback = 55;
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+
+console.log(typeof servicePercentPrice);
+console.log('Итоговая стоимость с учётом вычета отката будет равна ' + servicePercentPrice + 'р.');
+
+
+/*
+10) Написать конструкцию условий (расчеты приведены в рублях) (вывести в консоль)
+  - Если fullPrice больше 30000, то “Даем скидку в 10%”
+  - Если fullPrice больше 15000 и меньше 30000, то сообщение “Даем скидку в 5%”
+  - Если fullPrice меньше 15000 и больше 0 то в консоль вывести сообщение “Скидка не предусмотрена”
+  - Если отрицательное значение то вывести “Что то пошло не так”
+  - Учесть варианты 0, 15000 и 30000(к какому уровню не важно)
+  */
+switch (true) {
+    case fullPrice >= 30000:
+        console.log('Даем скидку в 10%');
+        break;
+    case 15000 <= fullPrice && fullPrice < 30000:
+        console.log('Даем скидку в 5%');
+        break;
+    case 0 < fullPrice && fullPrice < 15000:
+        console.log('Скидка не предусмотрена');
+        break;
+    default:
+        console.log('Что то пошло не так');
+}
+
+
+// console.log(screens.length);
+// console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+// console.log(screens);
+// console.log(fullPrice * (rollback / 100));
 
 // console.log('First log.');
 // alert('Important!');
