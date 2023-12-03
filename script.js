35dcfdc5cd94cf009bfb0a345ba42369684effff
SerAlexART <wolfi-5@mail.ru>
2023-12-03 10:47:49 +0300

    Выполнение 4-ого задания

--- script.js
+++ script.js
@@ -1,77 +1,92 @@
-// Привет! =) Знаю, что нужно изначально задавать переменные, а потом с ними работать, но в рамках выполнения домашних работ и их проверки, я решил временно разбить на блоки. Если это строго не рекомендуется, то просьба сообщить и я поправлю, спасибо!
+'use strict';
 
-
-
-// 3) Спрашиваем у пользователя “Как называется ваш проект?” и результат сохраняем в переменную title
 let title = prompt('Как называется ваш проект?');
-
-console.log('Название проекта ' + '"' + title + '".');
-
-
-
-//4) Спросить у пользователя “Какие типы экранов нужно разработать?” сохранить в переменную screens (пример: "Простые, Сложные, Интерактивные")
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные').toLocaleLowerCase().split(', ');
-
-console.log(screens);
-
-
-
-// 5) Спросить у пользователя “Сколько будет стоить данная работа?” и сохранить в переменную screenPrice (пример: 12000)
 let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
-
-console.log('Данная работа будет стоить ' + screenPrice + 'р.');
-
-
-// 6) Спросить у пользователя “Нужен ли адаптив на сайте?” и сохранить данные в переменной adaptive (булево значение true/false)
 let adaptiv = Boolean(prompt('Нужен ли адаптив на сайте?'));
-console.log(typeof adaptiv);
-console.log(adaptiv == true ? console.log('Адаптив нужен.') : console.log('Адаптив не нужен.'));
 
-
-// 7) Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные 1. “Какой дополнительный тип услуги нужен?” (например service1, service2) 2. “Сколько это будет стоить?” (например servicePrice1, servicePrice2) в итоге 4 вопроса и 4 разные переменных, вопросы задаются в такой последовательности Название - Стоимость - Название - Стоимость
 let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Вёрстка модального окна');
 let servicePrice1 = +prompt('Сколько это будет стоить?', '500');
 let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Написание скриптов для модального окна');
 let servicePrice2 = +prompt('Сколько это будет стоить?', '555');
-console.log(service1 + ' — стоимость ' + servicePrice1 + 'р.\n' + service2 + ' — стоимость ' + servicePrice2 + 'р.');
 
-
-// 8) Вычислить итоговую стоимость работы учитывая стоимость верстки экранов и дополнительных услуг (screenPrice + servicePrice1 + servicePrice2) и результат занести в переменную fullPrice
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-console.log(typeof fullPrice);
-console.log('Итоговая стоимость будет равна ' + fullPrice + 'р.');
-
-
-// 9) Объявить переменную servicePercentPrice и занести в нее итоговую стоимость за вычетом отката посреднику (servicePercentPrice = fullPrice - Откат посреднику), округлив результат в большую сторону (методы объекта Math в помощь). Вывести servicePercentPrice в консоль.
 let rollback = 55;
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
-
-console.log(typeof servicePercentPrice);
-console.log('Итоговая стоимость с учётом вычета отката будет равна ' + servicePercentPrice + 'р.');
 
 
-/*
-10) Написать конструкцию условий (расчеты приведены в рублях) (вывести в консоль)
-  - Если fullPrice больше 30000, то “Даем скидку в 10%”
-  - Если fullPrice больше 15000 и меньше 30000, то сообщение “Даем скидку в 5%”
-  - Если fullPrice меньше 15000 и больше 0 то в консоль вывести сообщение “Скидка не предусмотрена”
-  - Если отрицательное значение то вывести “Что то пошло не так”
-  - Учесть варианты 0, 15000 и 30000(к какому уровню не важно)
-  */
-switch (true) {
-    case fullPrice >= 30000:
-        console.log('Даем скидку в 10%');
-        break;
-    case 15000 <= fullPrice && fullPrice < 30000:
-        console.log('Даем скидку в 5%');
-        break;
-    case 0 < fullPrice && fullPrice < 15000:
-        console.log('Скидка не предусмотрена');
-        break;
-    default:
-        console.log('Что то пошло не так');
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+};
+
+// 1) Объявить функцию getAllServicePrices. Функция возвращает сумму всех дополнительных услуг. Результат сохраняем в переменную allServicePrices. Тип - function expression
+const getAllServicePrices = function (price1, price2) {
+    return price1 + price2;
+};
+
+let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+
+// 2) Объявить функцию getFullPrice. Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг (screenPrice + allServicePrices). Результат сохраняем в переменную fullPrice. Тип - function declaration
+function getFullPrice(price1, price2) {
+    return price1 + price2;
 }
+
+let fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+
+// 3) Объявить функцию getTitle. Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой". Учесть вариант что строка может начинаться с пустых символов. " КаЛьКулятор Верстки
+let getTitle = function (text) {
+    return text[0].toUpperCase() + text.substring(1).toLocaleLowerCase();
+};
+
+title = getTitle(title);
+
+// 4) Объявить функцию getServicePercentPrices. Функция возвращает итоговую стоимость за вычетом процента отката. Результат сохраняем в переменную servicePercentPrice (итоговая стоимость минус сумма отката)
+const getServicePercentPrices = function (fullPrice, rollback) {
+    return fullPrice - Math.ceil(fullPrice * (rollback / 100));
+};
+
+let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
+
+// Вариант через if
+const getRollbackMessage = function () {
+    if (fullPrice >= 30000) {
+        return 'Даем скидку в 10%';
+    } else if (fullPrice >= 15000 && fullPrice < 30000) {
+        return 'Даем скидку в 5%';
+    } else if (fullPrice > 0 && fullPrice < 15000) {
+        return 'Скидка не предусмотрена';
+    } else {
+        return 'Что то пошло не так';
+    }
+};
+
+// Вариант через switch
+const getRollbackMessageSwitch = function () {
+    switch (true) {
+        case fullPrice >= 30000:
+            return 'Даем скидку в 10%';
+            break;
+        // case 15000 <= fullPrice && fullPrice < 30000:
+        case fullPrice >= 15000 && fullPrice < 30000:
+            return 'Даем скидку в 5%';
+            break;
+        case 0 < fullPrice && fullPrice < 15000:
+            return 'Скидка не предусмотрена';
+            break;
+        default:
+            return 'Что то пошло не так';
+    }
+};
+
+
+showTypeOf('Название проекта ' + '"' + title + '".');
+showTypeOf(screens);
+showTypeOf('Данная работа будет стоить ' + screenPrice + 'р.');
+showTypeOf(getRollbackMessage());
+adaptiv == true ? showTypeOf('Адаптив нужен.') : showTypeOf('Адаптив не нужен.');
+showTypeOf('Итоговая стоимость будет равна ' + fullPrice + 'р.');
+showTypeOf('Итоговая стоимость с учётом вычета отката будет равна ' + servicePercentPrice + 'р.');
 
 
 // console.log(screens.length);
