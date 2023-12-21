a77546566ad39863a0fd16603079b0f0e48533ee
Sergey Alekseev <s_alekseev@lesta.group>
2023-12-21 17:25:50 +0300

    Внесение правок после комментариев

--- calc-project/js/script.js
+++ calc-project/js/script.js
@@ -1,6 +1,42 @@
 'use strict';
 
 // Объявление переменных
+
+// 1) Получить заголовок "Калькулятор верстки" через метод getElementsByTagName. (тэг h1, получить именно элемент, а не коллекцию)
+const title = document.getElementsByTagName('title')[0];
+
+// 2) Получить кнопки "Рассчитать" и "Сброс" через метод getElementsByClassName. (класс handler_btn)
+const handlerBtnCalculate = document.getElementsByClassName('handler_btn')[0];
+
+const handlerBtnReset = document.getElementsByClassName('handler_btn')[1];
+console.log(handlerBtnReset);
+// 3) Получить кнопку "+" под выпадающим списком через метод querySelector. (класс screen-btn)
+const screenBtn = document.querySelector('.screen-btn');
+
+// 4) Получить все элементы с классом other-items в две разные переменные. В первую элементы у которых так же присутствует класс percent, во вторую элементы у которых так же присутствует класс number через метод querySelectorAll.
+const otherItemsPercent = document.querySelectorAll('.other-items.percent');
+
+const otherItemsNumber = document.querySelectorAll('.other-items.number');
+
+// 5) Получить input type=range через его родителя с классом rollback одним запросом через метод querySelector.
+const inputRange = document.querySelector('.rollback input[type="range"]');
+
+// 6) Получить span с классом range-value через его родителя с классом rollback одним запросом через метод querySelector.
+const rangeValue = document.querySelector('.rollback span.range-value');
+
+// 7) Получить все инпуты с классом total-input справа через метод getElementsByClassName. (класс total-input, получить именно элементы, а не коллекции)
+const totalInput = document.getElementsByClassName('total-input')[0];
+const totalInputCount = document.getElementsByClassName('total-input')[1];
+const totalInputCountOther = document.getElementsByClassName('total-input')[2];
+const totalInputFullCount = document.getElementsByClassName('total-input')[3];
+const totalInputCountRollback = document.getElementsByClassName('total-input')[4];
+
+// 8) Получить все блоки с классом screen в изменяемую переменную ( let ) через метод querySelectorAll (далее мы будем переопределять ее значение)
+let screen = document.querySelectorAll('.screen');
+
+
+
+
 // Функция проверки типа
 // const showTypeOf = function (variable) {
 //     console.log(variable, typeof variable);
@@ -164,7 +200,6 @@ const appData = {
     // 3) Метод logger выводит в консоль необходимую информациюи запускается в самом конце метода start (после того как все расчеты уже были произведены)
     logger: function () {
         console.log('Название проекта ' + '"' + appData.title + '".');
-        // console.log('Типы экранов, которые нужно разработать: ' + appData.screens.join(', ') + '.');
 
         if (appData.adaptiv == true) {
             console.log('Адаптив нужен.');
@@ -176,67 +211,12 @@ const appData = {
         console.log('Итоговая стоимость будет равна ' + appData.fullPrice + 'р.');
         console.log('Итоговая стоимость с учётом вычета отката будет равна ' + appData.servicePercentPrice + 'р.');
         console.log(appData.screens);
-
-
-        // 4) Вывести в консоль из метода logger все свойства и методы объекта appData с помощью цикла for in
-        // for (let key in appData) {
-        //     console.log('Ключ ' + key + '. ' + 'Значение: ' + appData[key] + '.');
-        // }
     }
 
-    // Метод возвращает информацию в удобночитаемом формате
-    // loggerMessage: function () {
-    //     console.log('Название проекта ' + '"' + appData.title + '"');
-    //     console.log('Типы экранов, которые нужно разработать: ' + appData.screens.join(', ') + '.');
-
-    //     if (appData.adaptiv == true) {
-    //         console.log('Адаптив нужен.');
-    //     } else { console.log('Адаптив не нужен.'); }
-
-    //     console.log('Откат равен ' + appData.rollback + '%.');
-    //     console.log(appData.getRollbackMessage());
-    //     console.log('Итоговая стоимость дополнительных услуг будет равна ' + appData.allServicePrices + 'р.');
-    //     console.log('Итоговая стоимость будет равна ' + appData.fullPrice + 'р.');
-    //     console.log('Итоговая стоимость с учётом вычета отката будет равна ' + appData.servicePercentPrice + 'р.');
-    // }
 };
 
 // Вызов методов объекта appData
 // appData.start();
-
-// * Домашнее задание
-// 1) Получить заголовок "Калькулятор верстки" через метод getElementsByTagName. (тэг h1, получить именно элемент, а не коллекцию)
-const title = document.getElementById('title').textContent;
-
-// 2) Получить кнопки "Рассчитать" и "Сброс" через метод getElementsByClassName. (класс handler_btn)
-const handlerBtnCalculate = document.getElementsByClassName('handler_btn')[0];
-
-const handlerBtnReset = document.getElementsByClassName('handler_btn')[1];
-
-// 3) Получить кнопку "+" под выпадающим списком через метод querySelector. (класс screen-btn)
-const screenBtn = document.querySelector('.screen-btn');
-
-// 4) Получить все элементы с классом other-items в две разные переменные. В первую элементы у которых так же присутствует класс percent, во вторую элементы у которых так же присутствует класс number через метод querySelectorAll.
-const otherItemsPercent = document.querySelectorAll('.other-items.percent');
-
-const otherItemsNumber = document.querySelectorAll('.other-items.number');
-
-// 5) Получить input type=range через его родителя с классом rollback одним запросом через метод querySelector.
-const inputRange = document.querySelector('.rollback input[type="range"]');
-
-// 6) Получить span с классом range-value через его родителя с классом rollback одним запросом через метод querySelector.
-const rangeValue = document.querySelector('.rollback span.range-value');
-
-// 7) Получить все инпуты с классом total-input справа через метод getElementsByClassName. (класс total-input, получить именно элементы, а не коллекции)
-const totalInputs = document.getElementsByClassName('total-input');
-
-for (let input of totalInputs) {
-    console.log(input);
-}
-
-
-// 8) Получить все блоки с классом screen в изменяемую переменную ( let ) через метод querySelectorAll (далее мы будем переопределять ее значение)
-let screen = document.querySelectorAll('.screen');
 
 
 // Вывод значений в консоль
@@ -248,13 +228,9 @@ console.log(otherItemsPercent);
 console.log(otherItemsNumber);
 console.log(inputRange);
 console.log(rangeValue);
+console.log(totalInput);
+console.log(totalInputCount);
+console.log(totalInputCountOther);
+console.log(totalInputFullCount);
+console.log(totalInputCountRollback);
 console.log(screen);
-// console.log('Итоговая стоимость будет равна ' + appData.fullPrice + ', а с учётом вычета отката будет равна ' + appData.servicePercentPrice);
-
-// showTypeOf('Название проекта ' + '"' + title + '".');
-// showTypeOf('Типы экранов, которые нужно разработать: ' + screens + '.');
-// showTypeOf('Данная работа будет стоить ' + screenPrice + 'р.');
-// showTypeOf(getRollbackMessage());
-// adaptiv == true ? showTypeOf('Адаптив нужен.') : showTypeOf('Адаптив не нужен.');
-// showTypeOf('Итоговая стоимость будет равна ' + fullPrice + 'р.');
-// showTypeOf('Итоговая стоимость с учётом вычета отката будет равна ' + servicePercentPrice + 'р.');
