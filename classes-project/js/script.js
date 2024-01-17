f5b7d5402ae6ff5a64f417beaceff5b5c0a467a9
SerAlexART <wolfi-5@mail.ru>
2024-01-17 22:04:02 +0300

    Выполнение 16-ого задания

---
+++ classes-project/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class First {
+    constructor() {
+
+    }
+
+    hello() {
+        console.log(`Привет я метод родителя!`);
+    }
+}
+
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log(`А я наследуемый метод!`);
+    }
+}
+
+const message = new Second();
+
+message.hello();
